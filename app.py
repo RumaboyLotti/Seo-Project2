ab2859ceb433fe1985abe1b80c97f13685d71d98
RumaboyLotti <Oladimeji.bada18@gmail.com>
2023-07-19 09:42:13 -0500

    the route should work correctly

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect, request, jsonify
 from flask_behind_proxy import FlaskBehindProxy
-from sqlalchemy import create_engine, Column, String, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Integer, ForeignKey, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import random
@@ -12,7 +12,7 @@ from Password import *
 class Users(Base):
     __tablename__ = 'users'
 
-    user_name = Column('username', String, primary_key=True)
+    user_name = Column('username', String, primary_key=True,autoincrement=True)
     user_pwd = Column('password', String)
 
     def __init__(self, user_name, user_pwd):
@@ -23,30 +23,38 @@ class Users(Base):
         return f'\nUsername: {self.user_name}\n Password{self.user_pwd}'
 
 
+# Creating a class to represent a table in a database for storing passwords.
+# The class inherits from the `Base` class, which is typically associated with an ORM (Object-Relational Mapping) library like SQLAlchemy.
+
 class Passwords(Base):
     __tablename__ = 'passwords'
 
-    password_id = Column('password_id', Integer, primary_key=True)
+    # Defining the columns in the table
+    password_id = Column('password_id', Integer, primary_key=True, autoincrement=True)
     website = Column('website', String)
     password = Column('password', String)
     user = Column(Integer, ForeignKey('users.username'))
 
-    def __init__(self, pid, website, pwd, user):
-        self.password_id = pid
+    def __init__(self, website, pwd, user):
+        # Initializing the object with the provided values
         self.website = website
         self.password = pwd
         self.user = user
 
     def __repr__(self):
-        return f'\n({self.password_id})\n{self.website}\n{self.password}\n{self.user}'
+        # Defining the string representation of the object
+        return f'\n{self.website}\n{self.password}\n{self.user}'
 
 
+#specifying  db connections 
 app = Flask(__name__)
 engine = create_engine('sqlite:///project02.db')
 Base.metadata.create_all(bind=engine)
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
+#route to login and its methodolgy 
 @app.route('/', methods=['POST', 'GET'])
 def start_page():
     if request.method == 'POST':
@@ -77,16 +85,14 @@ def dashboard(user):
     return render_template('dashboard.html', user=user)
 
 
-#@app.route('/create_password/<user>', methods=['POST', 'GET'])
-@app.route('/create_password', methods=['POST', 'GET'])
+@app.route('/create_password<user>', methods=['POST', 'GET'])
 def create_password():
     if request.method == 'POST':
         print(json.request)
         return render_template('passwordCreator.html', text='saved the password')
     return render_template('passwordCreator.html')
 
-#@app.route('/randomPassword', methods=['POST'])
-#def random_password():
+
 @app.route('/randomPassword', methods=['GET', 'POST'])
 def random_password():
     length = int(request.form.get('len', 10))
@@ -106,85 +112,44 @@ def process_strings():
 
 def save_data(website, password):
     # Inserts the password into the database
-    new_entry = Passwords(50000, website, 'Milton')
+    new_entry = Passwords( website, 'Milton')
     try:
         session.add(new_entry)
         session.commit()
     except:
         i=1
 
-@app.route('/password_store/<user>')
-def password_store(user):
-    # this is a test entry
-    new_entry = Passwords(12345, 'https://codio.com', 'password', 'tochiboy')
-    new_new_entry = Passwords(54321, 'https://youtube.com', '12345678', 'tochiboy')
-    # adds the entries to the db
-    try:
-        session.add(new_entry)
-        session.add(new_new_entry)
-        session.commit()
-    except:
-        i = 1
+# @app.route('/password_store/<user>')
+# def password_store(user):
+# # this is a test entry
+# new_entry = Passwords(12345, 'https://codio.com', 'password', 'tochiboy')
+# new_new_entry = Passwords(54321, 'https://youtube.com', '12345678', 'tochiboy')
+# # adds the entries to the db
+# try:
+#     session.add(new_entry)
+#     session.add(new_new_entry)
+#     session.commit()
+# except:
+#     i = 1
 
-    # finds only the entries with the correct username attached
-    results = session.query(Passwords).where(Passwords.user == user)
-    string = ''
-    for r in results:
-        string += f'{r}\n'
-    # puts them all in one string and puts that on the show passwords
-    return render_template('passwordStore.html', text=string)
+# # finds only the entries with the correct username attached
+# results = session.query(Passwords).where(Passwords.user == user)
+# string = ''
+# for r in results:
+#     string += f'{r}\n'
+# # puts them all in one string and puts that on the show passwords
+# return render_template('passwordStore.html', text=string)
 
-@app.route('/show_password')
-def show_password():
-    random = "SELECT * FROM passwords "
-    p = ''
-    with engine.connect() as connection:   
-        p = connection.execute(db.text(random))
-        print(p.fetchall())
-    return 'success'
+@app.route('/password_show/<user>', methods=['GET'])
+# define a route show password and from there slect all passwords from the database and display it
+def show_password(user):
+    if request.method == 'GET':
+        query = "SELECT * FROM passwords WHERE user = :user"
+        with engine.connect() as connection:
+            result = connection.execute(text(query), user=user)
+            passwords = result.fetchall()
+    return render_template('passwordStore.html', text=passwords)
    
-
-
-# @app.route("/api", methods=["POST", "GET"])
-# def api():
-#     # print("backend grab form", request.form)
-#     # print("backend grab form per: ", request.form.get('username'), request.form.get('password'))
-#
-#     username = request.form.get('username')
-#     password = request.form.get('password')
-#     print(username)
-#
-#     valid_users = session.query(Users).all()
-#     if valid_users:
-#         for user in valid_users:
-#             if user.user_name == username:
-#                 print("username already exists!")
-#                 return jsonify(False)
-#     new_entry = Users(username, password)
-#     try:
-#         session.add(new_entry)
-#         session.commit()
-#     except:
-#         i = 1
-#     return jsonify(f"Username {username} added!")
-
-    # sql query to search for users with specific username
-    # if username exits , return jsonify string stating username exists
-    # if username in valid_users: # change to db name
-    #     print("username already exists!")
-    #     return jsonify(False)
-    #
-    # # If it doesn't exist in the set
-    # # insert into db both username and password .
-    # valid_users[username] = password
-    # return jsonify(f"Username {username} added!")
-
-    # flask redirect to passwordStore page  https://stackoverflow.com/questions/14343812/redirecting-to-url-in-flask
-    # return redirect(url_for('password_store'))
-
-    # https://flask-login.readthedocs.io/en/latest/
-
-
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
